daa95de959ef44d231b47ccba308edde4c340fca
vlmcdtv <vivian.moser12@gmail.com>
2018-12-25 16:08:18 -0800

    xmas day

--- tailwind.js
+++ tailwind.js
@@ -92,7 +92,7 @@ let colors = {
   "teal-darkest": "#0d3331",
   "teal-darker": "#20504f",
   "teal-dark": "#38a89d",
-  teal: "#4dc0b5",
+  teal: "#00997D",
   "teal-light": "#64d5ca",
   "teal-lighter": "#a0f0ed",
   "teal-lightest": "#e8fffe",
